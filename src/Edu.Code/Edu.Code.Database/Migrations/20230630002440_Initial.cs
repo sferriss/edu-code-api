60ef00856e830da9406c173746a1bf0d005f68f2
Sandro <sandro_junior@hotmail.com>
2023-06-29 21:26:14 -0300

    Feat: Migration

---
+++ src/Edu.Code/Edu.Code.Database/Migrations/20230630002440_Initial.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Edu.Code.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "edu-code");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "question_list",
+                schema: "edu-code",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_question_list", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "question",
+                schema: "edu-code",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    answer = table.Column<string>(type: "text", nullable: false),
+                    example = table.Column<string>(type: "text", nullable: true),
+                    difficulty = table.Column<int>(type: "integer", nullable: false),
+                    list_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_question", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_question_question_list_list_id",
+                        column: x => x.list_id,
+                        principalSchema: "edu-code",
+                        principalTable: "question_list",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_question_list_id",
+                schema: "edu-code",
+                table: "question",
+                column: "list_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "question",
+                schema: "edu-code");
+
+            migrationBuilder.DropTable(
+                name: "question_list",
+                schema: "edu-code");
+        }
+    }
+}
