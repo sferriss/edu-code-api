0f680805abda7ded0a8d4494c9315cc5783010b1
Sandro Ferri <sandro.ferri@cwi.com.br>
2023-06-30 09:05:38 -0300

    Fix: Tipo de retorno do endpoint

--- src/Edu.Code/Edu.Code.Api/Controllers/TeacherQuestionsController.cs
+++ src/Edu.Code/Edu.Code.Api/Controllers/TeacherQuestionsController.cs
@@ -5,17 +5,21 @@ using Microsoft.AspNetCore.Mvc;
 namespace Edu.Code.Api.Controllers;
 
 [ApiController]
-[Route("questions")]
-public class QuestionsController : ControllerBase
+[Route("teacher/questions")]
+public class TeacherQuestionsController : ControllerBase
 {
     private readonly IMediator _mediator;
 
-    public QuestionsController(IMediator mediator)
+    public TeacherQuestionsController(IMediator mediator)
     {
         _mediator = mediator;
     }
 
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(CreateQuestionListCommand))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<CreatedResult> PostCreateQuestionsListAsync([FromBody] CreateQuestionListCommand request)
     {
         var result = await _mediator.Send(request)
