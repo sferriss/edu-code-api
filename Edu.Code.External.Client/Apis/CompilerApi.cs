db6ebbb8253a539d3a1e2a0c44d3cc540df58b21
Sandro Ferri <sandro.ferri@cwi.com.br>
2023-07-05 19:07:38 -0300

    Feat: Jdoodle client request

---
+++ Edu.Code.External.Client/Apis/CompilerApi.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+using Edu.Code.External.Client.Extensions;
+using Edu.Code.External.Client.Requests.Compile;
+using Edu.Code.External.Client.Responses.Compile;
+
+namespace Edu.Code.External.Client.Apis;
+
+public class CompilerApi
+{
+    private readonly HttpClient _client;
+    private readonly string _clientId;
+    private readonly string _secret;
+
+    public CompilerApi(HttpClient client, string clientId, string secret)
+    {
+        _client = client;
+        _clientId = clientId;
+        _secret = secret;
+    }
+    
+    public async Task<CompileResponse?> PostExecuteAsync(CompileRequest request)
+    {
+        const string url = "v1/execute";
+        
+        var response = await _client.PostAsJsonAsync(url, BuildRequest(request))
+            .ConfigureAwait(false);
+        
+        var result = await response.Content.ReadAsJsonAsync<CompileResponse>()
+            .ConfigureAwait(false);
+        
+        return result;
+    }
+
+    private CompileRequest BuildRequest(CompileRequest request)
+    {
+        request.ClientSecret = _secret;
+        request.ClientId = _clientId;
+
+        return request;
+    }
+}
---
+++ Edu.Code.External.Client/HttpHandlers/EnsureSuccessCodeHandler.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+namespace Edu.Code.External.Client.HttpHandlers;
+
+public class EnsureSuccessCodeHandler : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var response = await base.SendAsync(request, cancellationToken);
+
+        ArgumentNullException.ThrowIfNull(response, nameof(response));
+
+        if (response.IsSuccessStatusCode)
+        {
+            return response;
+        }
+
+        var contentResponse = await response.Content.ReadAsStringAsync(cancellationToken);
+
+        Log.Error(
+            "Error: Request URL: {url}, Response Status: {status}, Response Content: {content}",
+            request.RequestUri,
+            response.StatusCode,
+            contentResponse);
+
+        throw new InvalidOperationException("Erro ao executar chamada externa");
+    }
+}
