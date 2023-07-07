174a753672e5b4590c9b298a30ae4eb28d58c338
Sandro Ferri <sandro.ferri@cwi.com.br>
2023-07-07 14:47:13 -0300

    fix: swagger

--- Edu.Code.Api/Program.cs
+++ Edu.Code.Api/Program.cs
@@ -15,14 +15,19 @@ builder.Services.AddExceptionHandlers()
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseCors(policies =>
 {
