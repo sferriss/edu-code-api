ae8f35077833757ee43f4003a89bb2b499214e21
Sandro Ferri <42790290+sferriss@users.noreply.github.com>
2023-07-07 08:50:22 -0300

    Feat: Implementação do client gpt (#3)
    * Feat: Implementação do client gpt
    
    * refactor: Ajustado classes de responses
    
    ---------
    
    Co-authored-by: Sandro Ferri <sandro.ferri@cwi.com.br>

---
+++ Edu.Code.External.Client/Apis/OpenAIApi.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+using Edu.Code.External.Client.Extensions;
+using Edu.Code.External.Client.Requests.OpenAI;
+using Edu.Code.External.Client.Responses.OpenAI;
+
+namespace Edu.Code.External.Client.Apis;
+
+public class OpenAiApi
+{
+    private readonly HttpClient _client;
+    private readonly string _model;
+    private readonly int _maxtoken;
+
+    public OpenAiApi(HttpClient client, string model, int maxtoken)
+    {
+        _client = client;
+        _model = model;
+        _maxtoken = maxtoken;
+    }
+    
+    public async Task<GptConversationResponse?> PostGptConversationAsync(GptConversationRequest conversationRequest)
+    {
+        const string url = "v1/chat/completions";
+
+        conversationRequest.Model = _model;
+        conversationRequest.Temperature = 0.7;
+        conversationRequest.MaxToken = _maxtoken;
+        
+
+        var response = await _client.PostAsJsonAsync(url, conversationRequest)
+            .ConfigureAwait(false);
+        
+        var result = await response.Content.ReadAsJsonAsync<GptConversationResponse>()
+            .ConfigureAwait(false);
+        
+        return result;
+    }
+}
---
+++ Edu.Code.External.Client/Responses/OpenAI/GptConversationResponse.cs
@@ -0,0 +1,16 @@
+﻿using Edu.Code.External.Client.Responses.OpenAI.Common;
+
+namespace Edu.Code.External.Client.Responses.OpenAI;
+
+public sealed class GptConversationResponse
+{
+    public string Id { get; set; } = null!;
+    
+    public string Object { get; set; } = null!;
+    
+    public long Created { get; set; }
+    
+    public List<ChoiceResponse> Choices { get; set; } = null!;
+    
+    public UsageResponse Usage { get; set; } = null!;
+}
